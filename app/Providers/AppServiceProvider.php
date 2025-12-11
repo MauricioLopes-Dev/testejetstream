afd022e50a0ae2b542c162aee965e71494b331ab
Mauricio Lopes da cunha <mauriciolc.codes@gmail.com>
2025-12-10 21:40:31 -0300

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
         // Se estiver em produção (Railway), força tudo para HTTPS
         if($this->app->environment('production')) {
             URL::forceScheme('https');
+            config(['fortify.home' => '/dashboard']);
         }
+        
     }
 }
